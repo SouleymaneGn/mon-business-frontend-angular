87326373d03df51431d259191029366f759c6b29
Lenovo T14s <souleymane14131@gmail.com>
2026-05-17 10:27:56 +0000

    chore(package) : install ng-openapi-gen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,6 +18,7 @@ import { VideosComponent } from './pages/ui-elements/videos/videos.component';
 import { SignInComponent } from './pages/auth-pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/auth-pages/sign-up/sign-up.component';
 import { CalenderComponent } from './pages/calender/calender.component';
+import { CustomerComponent } from './pages/customer/customer.component';
 
 export const routes: Routes = [
   {
@@ -30,6 +31,10 @@ export const routes: Routes = [
         pathMatch: 'full',
         title:
           'Angular Ecommerce Dashboard | TailAdmin - Angular Admin Dashboard Template',
+      },
+      {
+        path:'customers',
+        component:CustomerComponent
       },
       {
         path:'calendar',
